455dea420da26cbcd63e84382122eb9c1ef1bdbd
PrintCrizz <PrintCrizz@MacBook-Pro-de-Usuario.local>
2019-02-26 01:47:28 -0300

    update fadingOut/in

---
+++ Assets/LevelChangerScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChangerScript : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private int levelToLoad;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToNextLevel();
+        }
+
+    }
+
+    public void fadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+    public void FadeToNextLevel()
+    {
+        fadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
